3bac71d1f3582237ad88eb2228dabcdd1ba57415
Fynn <fynndayham@gmail.com>
2022-05-08 22:28:53 -0700

    Fynns first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+fynn day ham was here
+        </p>
+        <h1>
+          penis
+        
+        </h1>
+        <a
+          className="App-link"
+          href="https://youtube.com
+          "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
